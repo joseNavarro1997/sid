7982a8f3de5c2d049e1a9c10ba40d218ccae45a3
solanasnavarroi <735515@unizar.es>
2021-04-01 19:32:07 +0200

    Con un compañero, se ha incluído el código de ejemplo de la práctica incluída la línea <uses-permission android:name="android.permission.INTERNET" /> en el AndroidManifest.xml (esto último se explica en el guion).
    Signed-off-by: solanasnavarroi <735515@unizar.es>

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,67 @@
+
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+//package com.example.fbobillo.testapplication3;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+
+public class MainActivity extends AppCompatActivity
+{
+    private final static String TAG = "MainActivity";
+    private MyAsyncTask myTask = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+        // Para obtener el valor devuelto en onRetainCustomNonConfigurationInstance
+        myTask = (MyAsyncTask) getLastCustomNonConfigurationInstance();
+        if (myTask == null) {
+            // Evita crear una AsyncTask cada vez que, por ejemplo, hay una rotación
+            Log.i(TAG, "onCreate: About to create MyAsyncTask");
+            myTask = new MyAsyncTask(this);
+            myTask.execute("http://developer.android.com");
+        }
+        else
+            myTask.attach(this);
+        Toast.makeText(this, "Hola!", Toast.LENGTH_LONG).show();
+    }
+    /** Permite devolver un objeto y que persista entre cambios de configuración. Lo
+     invoca el sistema cuando se va a destruir una actividad y se sabe que se va a
+     crear otra nueva inmediatamente. Se llama entre onStop y onDestroy. */
+    @Override
+    public Object onRetainCustomNonConfigurationInstance()
+    {
+        // Además de devolver mi tarea, elimino la referencia en mActivity
+        myTask.detach();
+        // Devuelvo mi tarea, para que no se cree de nuevo cada vez
+        return myTask;
+    }
+    public void setupAdapter(Integer integer)
+    {
+        if (integer != -1)
+            Toast.makeText(MainActivity.this,
+                    "Codigo de respuesta: " + integer, Toast.LENGTH_LONG).show();
+    }
+}
+
+
+/*
+public class MainActivity extends AppCompatActivity {
+    //procedimiento
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
+*/
+
